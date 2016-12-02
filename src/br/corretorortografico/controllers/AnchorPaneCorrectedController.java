7ece75b54e141119eae22909d192bb6ba58daf29
fefer714 <fernandosoldera@hotmail.com>
2016-12-02 15:15:14 -0200

    commit
    listView

--- src/br/corretorortografico/controllers/AnchorPaneCorrectedController.java
+++ src/br/corretorortografico/controllers/AnchorPaneCorrectedController.java
@@ -1,6 +1,10 @@
 package br.corretorortografico.controllers;
 
+import java.awt.List;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
 import br.corretorortografico.models.Suggestions;
 import br.corretorortografico.models.Word;
@@ -9,34 +13,36 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
 
-public class AnchorPaneCorrectedController {
+public class AnchorPaneCorrectedController implements Initializable{
 	@FXML
-	private ListView<Word> listViewSuggestions;
+	private ListView<String> listViewSuggestions;
 	@FXML
 	private Button buttonNovaPalavra;
 	@FXML
 	private AnchorPane anchorPane;
 
-	private String[] wordsToBeCorrected;
-	private Suggestions suggestions;
-	
+	private static String[] wordsToBeCorrected;
+	private static Suggestions suggestions;
 
-	public void initData(String[] wordsToBeCorrected, Suggestions list) {
-		this.wordsToBeCorrected = wordsToBeCorrected;
-		this.suggestions = list;
-		
-		ObservableList<Word> items = FXCollections.observableArrayList(suggestions.getSuggestions());
-		
-		if(items.isEmpty()){
-			System.out.println("vazio");
-		}else{
-			//fazer mostrar na list view sugestoes
-			listViewSuggestions = new ListView<Word>(items);
-		}
+	public static String[] getWordsToBeCorrected() {
+		return wordsToBeCorrected;
+	}
+
+	public static void setWordsToBeCorrected(String[] wordsToBeCorrected) {
+		AnchorPaneCorrectedController.wordsToBeCorrected = wordsToBeCorrected;
+	}
+
+	public static Suggestions getSuggestions() {
+		return suggestions;
+	}
+
+	public static void setSuggestions(Suggestions suggestions) {
+		AnchorPaneCorrectedController.suggestions = suggestions;
 	}
 
 	@FXML
@@ -45,6 +51,25 @@ public class AnchorPaneCorrectedController {
 				.load(getClass().getResource("/br/corretorortografico/views/AnchorPaneToBeCorrected.fxml"));
 		this.anchorPane.getChildren().setAll(ap);
 
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		ArrayList<String> listWord = new ArrayList<String>();
+		
+		for(int i = 0; i < this.suggestions.getSuggestions().size(); i++){
+			listWord.add(suggestions.getSuggestions().get(i).getWord());
+		}
+		
+		ObservableList<String> items = FXCollections.observableArrayList(listWord);
+		
+		
+		if(items.isEmpty()){
+			System.out.println("vazio");
+		}else{
+			//fazer mostrar na list view sugestoes
+			listViewSuggestions.setItems(items);
+		}
 	}
 
 }
--- src/br/corretorortografico/controllers/AnchorPaneToBeCorrectedController.java
+++ src/br/corretorortografico/controllers/AnchorPaneToBeCorrectedController.java
@@ -1,16 +1,16 @@
 package br.corretorortografico.controllers;
 
 import java.io.IOException;
-import java.util.List;
-
 import br.corretorortografico.models.Spelling;
 import br.corretorortografico.models.Suggestions;
 import br.corretorortografico.models.Word;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class AnchorPaneToBeCorrectedController {
 	@FXML
@@ -23,18 +23,37 @@ public class AnchorPaneToBeCorrectedController {
 	@FXML
 	private void handleButtonCorrigir() throws IOException{
 		String[] wordsToBeCorrected = this.textAreaCorrigido.getText().split(" ");
-		
-		AnchorPane ap = (AnchorPane) FXMLLoader.load(getClass().getResource("/br/corretorortografico/views/AnchorPaneCorrected.fxml"));
-		AnchorPaneCorrectedController controller = new AnchorPaneCorrectedController();
-		
 		Spelling spell = new Spelling();
 		
 		Word wordToCorrect = new Word(); 
 		wordToCorrect.setWord(wordsToBeCorrected[0]);
 		
 		Suggestions list = spell.checkSpelling(wordToCorrect);
+		AnchorPaneCorrectedController.setWordsToBeCorrected(wordsToBeCorrected);
+		AnchorPaneCorrectedController.setSuggestions(list);
 		
-		controller.initData(wordsToBeCorrected, list);
-		this.anchorPane.getChildren().setAll(ap);
+		//AnchorPane ap = (AnchorPane) FXMLLoader.load(getClass().getResource("/br/corretorortografico/views/AnchorPaneCorrected.fxml"));
+		//AnchorPaneCorrectedController controller = new AnchorPaneCorrectedController();
+		
+		
+		
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(AnchorPaneCorrectedController.class.getResource("/br/corretorortografico/views/AnchorPaneCorrected.fxml"));
+		
+		AnchorPane page = (AnchorPane) loader.load();
+		
+		Stage stage = new Stage();
+		stage.setTitle("Sign Up");
+		
+		Scene scene = new Scene(page);
+		
+		stage.setScene(scene);
+		
+		//controller.initData(wordsToBeCorrected, list);
+		//AnchorPaneCorrectedController controller = loader.getController();
+		stage.showAndWait();
+		
+		
+		//this.anchorPane.getChildren().setAll(ap);
 	}
 }
